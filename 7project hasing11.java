31f40fd11b7f78833133d4e39a884f7068f74508
saifasaifa <saifa.pha@ku.th>
2025-09-08 13:13:58 +0700

    Update 7project hasing11.java

--- 7project hasing11.java
+++ 7project hasing11.java
@@ -1,40 +1,70 @@
 import java.util.Scanner;
+import java.util.InputMismatchException;
 
 public class Main {
-
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 
-		System.out.print("Enter size of hash table: ");
-		int size = sc.nextInt();
-		int[] table = new int[size];
+		int size = 0;
+		while (true) {
+			try {
+				System.out.print("Enter size of hash table: ");
+				size = sc.nextInt();
+				if (size <= 0) {
+					System.out.println("Size must be greater than 0. Try again.");
+				} else {
+					break;
+				}
+			} catch (InputMismatchException e) {
+				System.out.println("Error: Please enter a number.");
+				sc.next(); // clear wrong input
+			}
+		}
 
+		int[] table = new int[size];
 		for (int i = 0; i < size; i++) {
 			table[i] = -1;
 		}
 
-		int n;
+		int n = 0;
 		while (true) {
-			System.out.print("Enter number of elements to insert: ");
-			n = sc.nextInt();
-			if (n > size || n <= 0) {
-				System.out.println("Error: Number of elements must be between 1 and " + size + ". Try again.");
-			} else {
-				break; // valid input
+			try {
+				System.out.print("Enter number of elements to insert: ");
+				n = sc.nextInt();
+				if (n > size || n <= 0) {
+					System.out.println("=========================================");
+					System.out.println("Error: Number of elements must be between 1 and " + size);
+				} else {
+					break;
+				}
+			} catch (InputMismatchException e) {
+				System.out.println("=========================================");
+				System.out.println("Error: Please enter a number.");
+				sc.next();
 			}
 		}
-		
-		System.out.println("=====================================");
 
+        System.out.println("=========================================");
+            
 		int[] numbers = new int[n];
 		for (int i = 0; i < n; i++) {
-			System.out.print("Enter number " + (i + 1) + ": ");
-			numbers[i] = sc.nextInt();
+			while (true) {
+				try {
+                    
+					System.out.print("Enter number " + (i + 1) + ": ");
+					numbers[i] = sc.nextInt();
+					break;
+				} catch (InputMismatchException e) {
+					System.out.println("=========================================");
+					System.out.println("Error: Please enter a number.");
+					sc.next();
+				}
+			}
 		}
 
 		char choice;
 		while (true) {
-            System.out.println("=====================================");
+			System.out.println("=========================================");
 			System.out.println("Choose probing method:");
 			System.out.println("A. Linear Probing");
 			System.out.println("B. Quadratic Probing");
@@ -42,38 +72,36 @@ public class Main {
 			System.out.print("Your choice (A/B/C): ");
 			choice = sc.next().toUpperCase().charAt(0);
 			if (choice == 'A' || choice == 'B' || choice == 'C') break;
+			System.out.println("=========================================");
 			System.out.println("Invalid choice! Try again.");
 		}
 
 		if (choice == 'A') {
 			for (int num : numbers) insertLinear(table, num);
-			System.out.println("=====================================");
-			System.out.println("Result with Linear Probing:");
+			System.out.println("=========================================");
+			System.out.println("- Result with Linear Probing:");
 			printTable(table);
 		} else if (choice == 'B') {
 			for (int num : numbers) insertQuadratic(table, num);
-			System.out.println("=====================================");
-			System.out.println("Result with Quadratic Probing:");
+			System.out.println("=========================================");
+			System.out.println("- Result with Quadratic Probing:");
 			printTable(table);
-		} else { // choice C
+		} else {
 			int[] linearTable = new int[size];
 			int[] quadraticTable = new int[size];
 			for (int i = 0; i < size; i++) {
 				linearTable[i] = -1;
 				quadraticTable[i] = -1;
 			}
-
 			for (int num : numbers) {
 				insertLinear(linearTable, num);
 				insertQuadratic(quadraticTable, num);
 			}
-
-			System.out.println("=====================================");
-			System.out.println("Result with Linear Probing:");
+			System.out.println("=========================================");
+			System.out.println("- Result with Linear Probing:");
 			printTable(linearTable);
-
-			System.out.println("=====================================");
-			System.out.println("Result with Quadratic Probing:");
+			System.out.println("=========================================");
+			System.out.println("- Result with Quadratic Probing:");
 			printTable(quadraticTable);
 		}
 	}
@@ -85,7 +113,7 @@ public class Main {
 		while (table[index] != -1) {
 			index = (index + 1) % size;
 			if (index == startIndex) {
-				System.out.println("Table full, cannot insert " + key);
+				System.out.println("Linear Table full, cannot insert " + key);
 				return;
 			}
 		}
@@ -100,8 +128,8 @@ public class Main {
 		while (table[index] != -1) {
 			index = (hash + i * i) % size;
 			i++;
-			if (i == size) { 
-				System.out.println("Table full, cannot insert " + key);
+			if (i == size) {
+				System.out.println("Quadratic Table full, cannot insert " + key);
 				return;
 			}
 		}
@@ -109,7 +137,8 @@ public class Main {
 	}
 
 	public static void printTable(int[] table) {
-		System.out.println("Hash Table:");
+	    
+		System.out.println("+ Hash Table +");
 		for (int i = 0; i < table.length; i++) {
 			System.out.println("Index " + i + ": " + (table[i] == -1 ? "empty" : table[i]));
 		}
